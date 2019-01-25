1d2f701dc44c369bbdaf6543c417601b4f7dfbae
Kunal Singla <tekunalogy@gmail.com>
2019-01-25 15:12:00 -0800

    fixed stuff
    this should work

--- FrcProject_05/src/main/java/frc/team3647/robot/Motors.java
+++ FrcProject_05/src/main/java/frc/team3647/robot/Motors.java
@@ -1,4 +1,13 @@
-package org.usfirst.frc.team3647.robot;
+package frc.team3647.robot;
+
+import com.ctre.phoenix.motorcontrol.FeedbackDevice;
+import com.ctre.phoenix.motorcontrol.NeutralMode;
+import com.ctre.phoenix.motorcontrol.ControlMode;
+import com.ctre.phoenix.motorcontrol.can.WPI_TalonSRX;
+
+import edu.wpi.first.wpilibj.drive.DifferentialDrive;
+
+import com.ctre.phoenix.motorcontrol.can.VictorSPX;
 
 public class Motors 
 {
